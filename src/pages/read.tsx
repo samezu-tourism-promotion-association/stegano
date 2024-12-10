408329ab661aab7973c5ce84b83e2e8a2a034763
貝時計 KAITOKEI <m21148@g.metro-cit.ac.jp>
2024-12-10 15:45:29 +0900

    fix: read blacket

--- src/pages/read.tsx
+++ src/pages/read.tsx
@@ -62,176 +62,174 @@ export default function Read() {
         if (data.responses && data.responses[0].fullTextAnnotation) {
           setText(data.responses[0].fullTextAnnotation.text);
         }
-
         // 取得したテキストから、テンプレートIDを求める
         // もしテンプレートIDが含まれていないなら、ダイアログ表示(読み取りに失敗しました)
         setTemplateImagePath(getTemplatePath(findTemplateIdFromText(text!)));
       }
-    }
     } catch (error) {
-    console.error(error);
-  }
-};
-
-const decodeText = async () => {
-  if (text) {
-    setLoading(true);
-    try {
-      // request to https://opera7133--himitsu-fastapi-app-dev.modal.run/decode
-      // const res = await fetch(
-      //   "https://opera7133--himitsu-fastapi-app-dev.modal.run/decode",
-      //   {
-      //     method: "POST",
-      //     headers: {
-      //       "Content-Type": "application/json",
-      //     },
-      //     body: JSON.stringify({
-      //       cover_text: text.split("\n").slice(1).join(),
-      //       prompt: text.split("\n")[0],
-      //       language: "ja",
-      //       model_name: "leia-llm/Leia-Swallow-7b",
-      //       device: "cuda:0",
-      //     }),
-      //   }
-      // );
-      // const data = await res.json();
-      // setDecoded(data);
-      setDecoded(`2月の【VIP限定感謝フェア】では、〇〇様だけの特別価格をご案内させていただきます。
-
-また、ご来店の際には、専属のコンシェルジュがご案内をさせていただき、ゆっくりとお買い物をお楽しみいただけます。`);
-      setLoading(false);
-    } catch (error) {
-      setLoading(false);
       console.error(error);
     }
-  }
-};
+  };
 
-// 画像から読み取った文字列から、テンプレートIDを探す
-// テンプレートIDのフォーマット -> #(英字1文字)(数字1文字)
-const findTemplateIdFromText = (text: string): string[] => {
-  const keyChar: string = "#";  // keyとなる文字
-  const lengthOfId: number = 2; // IDの文字数
+  const decodeText = async () => {
+    if (text) {
+      setLoading(true);
+      try {
+        // request to https://opera7133--himitsu-fastapi-app-dev.modal.run/decode
+        // const res = await fetch(
+        //   "https://opera7133--himitsu-fastapi-app-dev.modal.run/decode",
+        //   {
+        //     method: "POST",
+        //     headers: {
+        //       "Content-Type": "application/json",
+        //     },
+        //     body: JSON.stringify({
+        //       cover_text: text.split("\n").slice(1).join(),
+        //       prompt: text.split("\n")[0],
+        //       language: "ja",
+        //       model_name: "leia-llm/Leia-Swallow-7b",
+        //       device: "cuda:0",
+        //     }),
+        //   }
+        // );
+        // const data = await res.json();
+        // setDecoded(data);
+        setDecoded(`2月の【VIP限定感謝フェア】では、〇〇様だけの特別価格をご案内させていただきます。
 
-  const candidates: string[] = []; // 候補となる文字列
-
-  // keyCharからlengthOfId文の文字を文字列として取得
-  for (const char of text) {
-    if (candidates[candidates.length - 1].length < lengthOfId) {
-      candidates[candidates.length - 1] += char;
+また、ご来店の際には、専属のコンシェルジュがご案内をさせていただき、ゆっくりとお買い物をお楽しみいただけます。`);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        console.error(error);
+      }
     }
-    if (char == keyChar) {
-      candidates.push("");
+  };
+
+  // 画像から読み取った文字列から、テンプレートIDを探す
+  // テンプレートIDのフォーマット -> #(英字1文字)(数字1文字)
+  const findTemplateIdFromText = (text: string): string[] => {
+    const keyChar: string = "#";  // keyとなる文字
+    const lengthOfId: number = 2; // IDの文字数
+
+    const candidates: string[] = []; // 候補となる文字列
+
+    // keyCharからlengthOfId文の文字を文字列として取得
+    for (const char of text) {
+      if (candidates[candidates.length - 1].length < lengthOfId) {
+        candidates[candidates.length - 1] += char;
+      }
+      if (char == keyChar) {
+        candidates.push("");
+      }
     }
-  }
 
-  return candidates;
-};
+    return candidates;
+  };
 
-// テンプレートIDの候補から、テンプレート画像のPathを取得
-const getTemplatePath = (candidates: string[]): string => {
-  // id候補がtemplatesに含まれているかを確かめる
-  // もし複数含まれている場合なら、最初に"含まれている"と判断したPathを返す
-  for (const candidate of candidates) {
-    if (candidate in templates) {
-      return templates[candidate as string] as string;
+  // テンプレートIDの候補から、テンプレート画像のPathを取得
+  const getTemplatePath = (candidates: string[]): string => {
+    // id候補がtemplatesに含まれているかを確かめる
+    // もし複数含まれている場合なら、最初に"含まれている"と判断したPathを返す
+    for (const candidate of candidates) {
+      if (candidate in templates) {
+        return templates[candidate as string] as string;
+      }
     }
-  }
 
-  return "Not Found";
-};
+    return "Not Found";
+  };
 
-return (
-  <Layout>
-    <NextSeo title="よむ - ステ賀乃" />
-    <Header back="/" title="よむ" />
-    <Dialog
-      open={loading}
-      onClose={() => { }}
-      transition
-      className="fixed inset-0 flex w-screen items-center justify-center bg-black/50 p-4 transition duration-300 ease-out data-[closed]:opacity-0"
-    >
-      <DialogPanel className="rounded-lg max-w-lg bg-white p-4">
-        <div className="py-8 px-8 flex flex-col items-center gap-6">
-          <RiLoader4Fill size={72} className="animate-spin" />
-          <DialogTitle className="font-bold text-xl font-kaisei">
-            コトバを戻しています
-          </DialogTitle>
-        </div>
-      </DialogPanel>
-    </Dialog>
-    <main className="max-w-4xl mx-auto pb-16">
-      {image ? (
-        <>
-          <div className="relative max-w-lg mx-auto aspect-square">
-            {typeof image === "string" && (
-              <img
-                src={image}
-                alt="Captured"
-                className="object-cover w-full h-full"
-              />
-            )}
+  return (
+    <Layout>
+      <NextSeo title="よむ - ステ賀乃" />
+      <Header back="/" title="よむ" />
+      <Dialog
+        open={loading}
+        onClose={() => { }}
+        transition
+        className="fixed inset-0 flex w-screen items-center justify-center bg-black/50 p-4 transition duration-300 ease-out data-[closed]:opacity-0"
+      >
+        <DialogPanel className="rounded-lg max-w-lg bg-white p-4">
+          <div className="py-8 px-8 flex flex-col items-center gap-6">
+            <RiLoader4Fill size={72} className="animate-spin" />
+            <DialogTitle className="font-bold text-xl font-kaisei">
+              コトバを戻しています
+            </DialogTitle>
           </div>
-          {text && (
-            <div className="px-4 my-4">
-              <p className="font-bold mt-4 text-lg">
-                読み取り結果はあっていますか？
-              </p>
-              <textarea
-                className="w-full p-3 my-4 text-black bg-gray-100 focus:ring-primary focus:border-primary rounded-lg"
-                readOnly
-                rows={5}
-              >
-                {text}
-              </textarea>
+        </DialogPanel>
+      </Dialog>
+      <main className="max-w-4xl mx-auto pb-16">
+        {image ? (
+          <>
+            <div className="relative max-w-lg mx-auto aspect-square">
+              {typeof image === "string" && (
+                <img
+                  src={image}
+                  alt="Captured"
+                  className="object-cover w-full h-full"
+                />
+              )}
             </div>
-          )}
-          <div className="px-4 flex flex-col gap-4 my-4">
-            {text ? (
-              <Button
-                onClick={() => { }}
-                text="コトバに戻す"
-                icon={RiFileTextLine}
-                className="w-full px-4"
-                bgColor="bg-secondary"
-              />
-            ) : (
-              <Button
-                onClick={() => analyzeImage()}
-                text="解析"
-                icon={RiMenuSearchLine}
-                className="w-full px-4"
-                bgColor="bg-secondary"
-              />
+            {text && (
+              <div className="px-4 my-4">
+                <p className="font-bold mt-4 text-lg">
+                  読み取り結果はあっていますか？
+                </p>
+                <textarea
+                  className="w-full p-3 my-4 text-black bg-gray-100 focus:ring-primary focus:border-primary rounded-lg"
+                  readOnly
+                  rows={5}
+                >
+                  {text}
+                </textarea>
+              </div>
             )}
-            <Button
-              onClick={() => setImage(undefined)}
-              text="やり直し"
-              icon={RiRestartLine}
-              className="w-full px-4"
-            />
-          </div>
-        </>
-      ) : (
-        <>
-          <div className="relative max-w-lg mx-auto aspect-square">
-            <Camera
-              ref={camera}
-              errorMessages={{}}
-              facingMode="environment"
-            />
-          </div>
-          <div className="px-4">
-            <Button
-              onClick={() => setImage(camera.current?.takePhoto())}
-              text="撮影"
-              icon={RiCamera3Line}
-              className="w-full px-4 my-8"
-            />
-          </div>
-        </>
-      )}
-    </main>
-  </Layout>
-);
+            <div className="px-4 flex flex-col gap-4 my-4">
+              {text ? (
+                <Button
+                  onClick={() => { }}
+                  text="コトバに戻す"
+                  icon={RiFileTextLine}
+                  className="w-full px-4"
+                  bgColor="bg-secondary"
+                />
+              ) : (
+                <Button
+                  onClick={() => analyzeImage()}
+                  text="解析"
+                  icon={RiMenuSearchLine}
+                  className="w-full px-4"
+                  bgColor="bg-secondary"
+                />
+              )}
+              <Button
+                onClick={() => setImage(undefined)}
+                text="やり直し"
+                icon={RiRestartLine}
+                className="w-full px-4"
+              />
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="relative max-w-lg mx-auto aspect-square">
+              <Camera
+                ref={camera}
+                errorMessages={{}}
+                facingMode="environment"
+              />
+            </div>
+            <div className="px-4">
+              <Button
+                onClick={() => setImage(camera.current?.takePhoto())}
+                text="撮影"
+                icon={RiCamera3Line}
+                className="w-full px-4 my-8"
+              />
+            </div>
+          </>
+        )}
+      </main>
+    </Layout>
+  );
 }
