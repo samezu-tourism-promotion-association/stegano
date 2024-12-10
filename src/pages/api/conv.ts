e21c7be5911b8a6044931acbb44600ade52d96ba
wamo <ainznino@pm.me>
2024-12-10 15:06:13 +0900

    api

---
+++ src/pages/api/conv.ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  function buf2bin(buffer: Buffer): string {
+    return BigInt("0x" + buffer.toString("hex"))
+      .toString(2)
+      .padStart(buffer.length * 8, "0");
+  }
+  const { text } = req.body;
+  const binaryText = Buffer.from(text, "utf16le");
+  const binary = buf2bin(binaryText);
+  res.status(200).json({ binary });
+}
