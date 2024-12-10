9d63d38ef6f8fd913cb2197f8f1cc4883ac3bc39
貝時計 KAITOKEI <m21148@g.metro-cit.ac.jp>
2024-12-10 15:57:43 +0900

    add: templates

--- src/lib/templates.ts
+++ src/lib/templates.ts
@@ -7,6 +7,9 @@ const templates: templateType = {
     "#R0": "/red_sample.svg",
     "#B0": "/blue_sample.svg",
     "#Y0": "/yellow_sample.svg",
+    "#S0": "/sunrise.svg",
+    "#T0": "/snake.svg",
+    "#F0": "/mtfuji.svg",
 };
 
 export default templates;
