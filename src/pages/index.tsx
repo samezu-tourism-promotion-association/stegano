eaed6bf5c990a90b286c7928860d01ce698d1dac
Itsuki54 <5shi.icyu@gmail.com>
2024-12-10 14:01:34 +0900

    cookieから取得完了

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
 import Layout from "@/layouts/Layout";
 import { NextSeo } from "next-seo";
 import Header from "@/components/Header";
-import Letter from "@/components/Letter";
+import Letter from "@/components/LetterPreview";
 import Footer from "@/components/Footer";
+import { getCookie } from "cookies-next/server";
+import { GetServerSideProps } from 'next';
 
-export default function Home() {
+interface HomeProps {
+  token: string | null;
+}
+
+export default function Home({ token }: HomeProps) {
+  const letters:Letter[] = token ? JSON.parse(token) : [];
+
   return (
     <Layout>
       <NextSeo title="ステ賀乃" />
@@ -13,25 +21,37 @@ export default function Home() {
         <div className="pt-12">
           <h2 className="text-2xl font-bold">つくった年賀状</h2>
           <div className="mt-8 flex items-start gap-4 overflow-x-scroll">
-            <Letter blank />
-            <Letter title="テスト" image="https://picsum.photos/512/512" />
-            <Letter title="テスト" image="https://picsum.photos/512/512" />
-            <Letter title="テスト" image="https://picsum.photos/512/512" />
-            <Letter title="テスト" image="https://picsum.photos/512/512" />
+            {letters.length ? (
+              letters.map((letter: Letter) => (
+                <Letter
+                  key={letter.id}
+                  type={letter.type as unknown as number }
+                  text={letter.text}
+                  id={letter.id}
+                />
+              ))
+            ) : (
+              <p>年賀状はありません。</p>
+            )}
           </div>
         </div>
         <div className="pt-12">
           <h2 className="text-2xl font-bold">読んだ年賀状</h2>
           <div className="mt-8 flex items-start gap-4 overflow-x-scroll">
-            <Letter title="テスト" image="https://picsum.photos/512/512" />
-            <Letter title="テスト" image="https://picsum.photos/512/512" />
-            <Letter title="テスト" image="https://picsum.photos/512/512" />
-            <Letter title="テスト" image="https://picsum.photos/512/512" />
-            <Letter title="テスト" image="https://picsum.photos/512/512" />
           </div>
         </div>
       </main>
       <Footer />
     </Layout>
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const cookie = await getCookie("created", { req: context.req, res: context.res });
+  console.log(cookie);
+  return {
+    props: {
+      token: cookie ?? null,
+    },
+  };
 }
